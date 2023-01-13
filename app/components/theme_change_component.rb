0be8660ad6146836c576bdee8ce06df45201a200
jisuanjixue <2078350663@qq.com>
2023-01-13 17:02:04 +0800

    refactor theme component

---
+++ app/components/theme_change_component.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+class ThemeChangeComponent < ViewComponentReflex::Component
+  def themes
+    [
+          "light",
+          "dark",
+          "cupcake",
+          "bumblebee",
+          "emerald",
+          "corporate",
+          "synthwave",
+          "retro",
+          "cyberpunk",
+          "valentine",
+          "halloween",
+          "garden",
+          "forest",
+          "aqua",
+          "lofi",
+          "pastel",
+          "fantasy",
+          "wireframe",
+          "black",
+          "luxury",
+          "dracula",
+          "cmyk",
+          "autumn",
+          "business",
+          "acid",
+          "lemonade",
+          "night",
+          "coffee",
+          "winter",
+        ]
+  end
+end
