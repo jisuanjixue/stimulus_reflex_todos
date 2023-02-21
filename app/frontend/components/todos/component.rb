6471ab6d0d11e50e75b72d1146e69bae0715e0b3
jisuanjixue <2078350663@qq.com>
2023-02-21 17:09:13 +0800

    fix add todo error

--- app/frontend/components/todos/component.rb
+++ app/frontend/components/todos/component.rb
@@ -29,9 +29,9 @@ class Todos::Component < ApplicationViewComponent
 
   def filter_class(filter)
     if @filter == filter
-      "btn-primary"
+      "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
     else
-      "btn-secondary"
+      "text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-800"
     end
   end
 
